96a681455ec8b396ab99b5617a57857530e206c4
LangusMcgee <77115641+LangusMcgee@users.noreply.github.com>
2025-05-08 15:34:02 +0100

    Particles working

--- Dungeon GSP/Assets/Scripts/Weapons/PlayerWeapons/ThrowAble.cs
+++ Dungeon GSP/Assets/Scripts/Weapons/PlayerWeapons/ThrowAble.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class ThrowAble : WeaponBase
 {
-    public Transform throwOrigin;
     public GameObject grenade;
-    public float throwForce = 20f;
+    public GameObject fireBall;
+    public float fireBallSpeed = 20f;
+    public float GrenadeThrowForce = 20f;
     // Start is called before the first frame update
     public override void handleInput()
     {
-        if ((Input.GetKeyDown(KeyCode.Mouse1) || Input.GetKeyDown(KeyCode.Mouse0)) && Time.time >= nextFireTime) // only allow fire if enough time as passed
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time >= nextFireTime) // only allow fire if enough time as passed
         {
             nextFireTime = Time.time + fireRate; // Set next fire time
+            MainFire();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            nextFireTime = Time.time + fireRate;
             AltFire();
         }
     }
 
+    public override void MainFire()
+    {
+        ShootProjectile(fireBall, weaponOrigin, fireBallSpeed);
+    }
+
     public override void AltFire()
     {
-        ThrowProjectile(throwForce, grenade, throwOrigin);
+        ThrowProjectile(GrenadeThrowForce, grenade, weaponOrigin);
         shotTheGun?.Invoke(); // Invoke UnityEvent for effects like gun recoil
     }
 
--- Dungeon GSP/Assets/Scripts/Weapons/WeaponBase.cs
+++ Dungeon GSP/Assets/Scripts/Weapons/WeaponBase.cs
@@ -13,6 +13,7 @@ public class WeaponBase : MonoBehaviour
     public float fireRate = 0.5f; // Fire rate in seconds
     protected float nextFireTime = 0f; // Tracks when the weapon can fire again
     protected Vector3 attackPoint = new Vector3(0, 0, 1f);
+    public GameObject hitEffectPrefab; // Assign this in the Inspector
 
 
     private void Start()
@@ -33,9 +34,8 @@ public class WeaponBase : MonoBehaviour
 
     public virtual void handleInput()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time >= nextFireTime) // only allow fire if enough time as passed
+        if (Input.GetKey(KeyCode.Mouse0)) // only allow fire if enough time as passed
         {
-            nextFireTime = Time.time + fireRate; // Set next fire time
             MainFire();
         }
     }
@@ -49,9 +49,15 @@ public class WeaponBase : MonoBehaviour
         if (Physics.Raycast(ray, out hit, attackRange))
         {
             Debug.Log("Hit Entity");
+
             Damagable damagable = hit.collider.GetComponent<Damagable>();
             if (damagable != null)
             {
+                if (hitEffectPrefab != null)
+                {
+                    GameObject hitEffect = Instantiate(hitEffectPrefab, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(hitEffect, 2f);
+                }
                 Debug.Log("Damaged Entity");
                 damagable.TakeDamage(damage); // if the other collision is damagable take damage
             }
@@ -65,11 +71,29 @@ public class WeaponBase : MonoBehaviour
         Rigidbody projectileRB = projectile.GetComponent<Rigidbody>();
         projectileRB.AddForce(_throwOrigin.transform.forward * _forceAmount);
     }
+
+    public void ShootProjectile(GameObject _projectile, Transform _throwOrigin, float _speed)
+    {
+        float spawnOffset = 1f;
+        GameObject projectile = Instantiate(_projectile, _throwOrigin.transform.position + _throwOrigin.transform.forward * spawnOffset, _throwOrigin.rotation);
+        Rigidbody projectileRB = projectile.GetComponent <Rigidbody>();
+
+        if(projectileRB != null)
+        {
+            projectileRB.useGravity = false; // Don't want it affected by gravity as it shoots straight.
+            projectileRB.velocity = _throwOrigin.forward * _speed;
+        }
+    }
+
     public virtual void MainFire()
     {
-        Debug.Log("Fired A Shot");
-        RayDamage(weaponOrigin.position, weaponOrigin.forward);
-        shotTheGun?.Invoke(); // Invoke UnityEvent for effects like gun recoil
+        if(Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate; // Set next fire time
+            Debug.Log("Fired A Shot");
+            RayDamage(weaponOrigin.position, weaponOrigin.forward);
+            shotTheGun?.Invoke(); // Invoke UnityEvent for effects like gun recoil
+        }
     }
 
     public virtual void AltFire()
